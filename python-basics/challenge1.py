5843c4d74b8c4e49907ae4cc7a0ee16a38a37232
cyrilbaraka <emmanuelgift818@gmail.com>
2026-02-11 09:21:21 +0300

    first commit

---
+++ python-basics/challenge1.py
@@ -0,0 +1,9 @@
+university="KSOF"
+grade="B+"
+favourite_course="Flying"
+cluster_points="73"
+
+print(f"My favourite university is {university}")
+print(f"My grade is {grade}")
+print(f"My preferred course is {favourite_course}")
+print(f"My cluster points are {cluster_points}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+#This is my name
+print("hello to Cyril")
+#City
+print("I'm from Nairobi")
+#Hobbies
+print("I like to listen to music,play videogames and sleep")
+
+age=17
+height=1.7
+weight=75
+
+print(f" I am {age} years old")
+print(f" I am also {height} metres tall")
+print(f" And I weigh {weight} kilograms")
