1f4c68d3657554522fb6b505c7e36e022e300503
cyrilbaraka <emmanuelgift818@gmail.com>
2026-02-11 10:38:54 +0300

    Learnt about input

---
+++ python-basics/numbers.py
@@ -0,0 +1,19 @@
+#Name :Cyril Baraka
+#Date :11/02/2026
+#A program to add,subtract,divide and multiply two numbers from input
+
+first_number=int(input("Enter the first number"))
+second_number=int(input("Enter the second number"))
+
+sum_numbers=first_number+second_number
+difference_numbers=second_number-first_number
+quotient_numbers=second_number/first_number
+product_numbers=first_number*second_number
+
+print(f"The sum of the numbers is{sum_numbers}")
+print(f"The difference of the numbers is{difference_numbers}")
+print(f"The quotient of the numbers is{quotient_numbers}")
+print(f"The product of the numbers is{product_numbers}")
+
+
+
---
+++ python-basics/user_input.py
@@ -0,0 +1,6 @@
+#Name :Cyril Baraka
+#Date :11/02/2026
+#A program to get user input
+name=input("What is your name?")
+
+print(name)
